a559817e23e5fd501c385e96134d3724657bbaf8
anhari11 <althaia.control@gmail.com>
2025-10-27 10:26:29 +0100

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -287,8 +287,8 @@ export default function Header() {
         {/* Left: Brand */}
         <button onClick={() => router.push("/")} style={{ display: "flex", alignItems: "center", gap: 8 }}>
           <Image 
-            width={isMobile ? 60 : 100} 
-            height={isMobile ? 60 : 100} 
+            width={isMobile ? 60 : 90} 
+            height={isMobile ? 60 : 900} 
             alt="Webpage logo" 
             src={'/logoheader.png'} 
           />
