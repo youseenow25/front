819da1f11afe3a03c0dc61b6eec52295ddb88e87
anhari11 <althaia.control@gmail.com>
2025-11-12 17:33:38 +0100

    pipeline desde scm

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/api/',
     },
-    sitemap: 'https://hubreceipts.com/sitemap.xml',
+    sitemap: 'https://www.hubreceipts.com/sitemap.xml',
   }
 }
