1216c4067e3d0dafdd8d7f7ffc70bbf7d466a92f
makalel <118302945+anhari11@users.noreply.github.com>
2026-04-10 11:43:27 +0200

    change

--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -69,7 +69,6 @@ interface HeroProps {
 
 export default function Hero({ brandName = "" }: HeroProps) {
   const phrases = [
-    "🏆 Top #1 receipt generator ",
     "🌍 +6500 users worlwide using it",
   ];
 
